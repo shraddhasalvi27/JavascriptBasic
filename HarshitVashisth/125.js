9f594477a6c93d87ec23a42b057c87532e9c62e2
shraddhasalvi27 <shraddhasalvi272@gmail.com>
2024-07-04 22:35:38 +0530

    events asynchronous js and promises http request

---
+++ HarshitVashisth/125.js
@@ -0,0 +1,11 @@
+// Callbacks in asynchronous programming 
+// to maintain using settimeout is difficult 
+// have to change the timig order of every function
+// one more way is settimeout inside settimeout - that is a callback hell
+
+
+//we can do it using callback functions
+
+// right function and give callback function again and agian
+
+// callback hell and pyramid of dom
